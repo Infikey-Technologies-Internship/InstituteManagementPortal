bfef70a2493267f8e432146919b7b318112381a2
Dhvanil16 <jaydipadhiyar27@gmail.com>
2022-10-02 17:51:41 +0530

    Pushed on Github

---
+++ client/build/precache-manifest.8c3a5d586e9dd0aed1f687313abe9e8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60bbd13052a08555af90bc7f9e2863dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1f49f706cebcbadadff",
+    "url": "/static/css/main.18cb0288.chunk.css"
+  },
+  {
+    "revision": "e0b3901b7b0c5f983def",
+    "url": "/static/js/2.00c17631.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.00c17631.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1f49f706cebcbadadff",
+    "url": "/static/js/main.fb9c5784.chunk.js"
+  },
+  {
+    "revision": "4fddd1679e3464d3764c",
+    "url": "/static/js/runtime-main.d29c5a69.js"
+  },
+  {
+    "revision": "0e651ff6c9928dad413931ce4ed2057b",
+    "url": "/static/media/green-chameleon-s9CC2SKySJM-unsplash.0e651ff6.jpg"
+  }
+]);
